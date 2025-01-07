046b7bfb69d85bd04c273f3f702c745e8210c922
tenoriopedro <pstsouza13@gmail.com>
2025-01-07 22:45:25 +0000

    Finishing the Page Model

--- Blog/djangoapp/blog/views.py
+++ Blog/djangoapp/blog/views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.db.models import Q
-from blog.models import Post
+from blog.models import Post, Page
 
 PER_PAGE = 9
 
@@ -91,12 +91,19 @@ def search(request):
 
 
 def page(request, slug):
+    page = (
+        Page.objects
+        .filter(is_published=True)
+        .filter(slug=slug)
+        .first()
+    )
+
 
     return render(
         request,
         'blog/pages/page.html',
         {
-            # 'page_obj': page_obj,
+            'page': page,
         }
     )
 
